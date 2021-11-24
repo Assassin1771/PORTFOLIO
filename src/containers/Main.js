f7ae0b6988cffee85bb8e9c0fae432b093c98acd
Tushar Garg <tshrgarg2010@gmail.com>
2021-11-24 20:53:19 +0530

    Adding Dark Mode button text & removing Footer

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -15,50 +15,35 @@ import Education from "./education/Education";
 import Top from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
 import Profile from "./profile/Profile";
-import { StyleProvider } from "../contexts/StyleContext";
-import { useLocalStorage } from "../hooks/useLocalStorage";
+import {StyleProvider} from "../contexts/StyleContext";
+import {useLocalStorage} from "../hooks/useLocalStorage";
 import "./Main.scss";
 
 const Main = () => {
-    const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
-    const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
+  const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
+  const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
 
-    const changeTheme = () => {
-        setIsDark(!isDark);
-    };
+  const changeTheme = () => {
+    setIsDark(!isDark);
+  };
 
-    return ( <
-        div className = { isDark ? "dark-mode" : null } >
-        <
-        StyleProvider value = {
-            { isDark: isDark, changeTheme: changeTheme }
-        } >
-        <
-        Header / >
-        <
-        Greeting / >
-        <
-        Skills / >
-        <
-        StackProgress / >
-        <
-        Education / >
-        <
-        WorkExperience / >
-        <
-        Projects / >
-        <
-        StartupProject / >
-        <
-        Achievement / >
-        <
-        Profile / >
-        <
-        Top / >
-        <
-        /StyleProvider> < /
-        div >
-    );
+  return (
+    <div className={isDark ? "dark-mode" : null}>
+      <StyleProvider value={{isDark: isDark, changeTheme: changeTheme}}>
+        <Header />
+        <Greeting />
+        <Skills />
+        <StackProgress />
+        <Education />
+        <WorkExperience />
+        <Projects />
+        <StartupProject />
+        <Achievement />
+        <Profile />
+        <Top />
+      </StyleProvider>
+    </div>
+  );
 };
 
 export default Main;
